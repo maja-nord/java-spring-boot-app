5777b78cb73dbd71a2257c69b5760cce996a2fe6
Maja Nord <maja.nord@protonmail.com>
2020-11-25 19:33:21 +0100

    First version

---
+++ src/main/java/maja_nord/java_spring_boot_app/JavaSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package maja_nord.java_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maja_nord/java_spring_boot_app/JavaSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package maja_nord.java_spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
